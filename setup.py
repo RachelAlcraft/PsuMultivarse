2cdfc04bb6a9e337a436b04d6d4801c55941b536
Rachel A <rachelalcraft@gmail.com>
2021-01-15 11:01:00 +0000

    Skeleton

---
+++ setup.py
@@ -0,0 +1,17 @@
+
+
+'''
+https://python-packaging.readthedocs.io/en/latest/minimal.html
+'''
+
+from setuptools import setup
+
+setup(name='PsuMultivarse',
+      version='1.0',
+      description='Explore protein structure geometry',
+      url='https://github.com/RachelAlcraft/PsuMultivarse',
+      author='Rachel Alcraft',
+      author_email='rachelalcraft@gmail.com',
+      license='MIT',
+      packages=['PsuMultivarse'],
+      zip_safe=False)
